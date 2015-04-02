93b39d28eb937e10c0d8e447d78cf9c2023bd3cc
CJ Avilla <cjavilla@gmail.com>
2015-04-01 22:16:04 -0700

    go to /

--- js/controllers/login_controller.js
+++ js/controllers/login_controller.js
@@ -1,11 +1,11 @@
 angular.module('insider.controllers')
-  .controller('LoginCtrl', function ($state, $scope, $window, LoginService) {
+  .controller('LoginCtrl', function ($state, $scope, $location, LoginService) {
     $scope.user = {};
     $scope.errorMessage = "";
     $scope.doLogin = function () {
       $scope.errorMessage = '';
       LoginService.login($scope.user).then(function () {
-        $window.history.back();
+        $location.path("/");
       }, function (resp) {
         $scope.errorMessage = resp.data.message;
       });
